f270f2740a15e04208256879e93aa2c801dbb1b6
Dan Turkel <daturkel@gmail.com>
2020-11-19 16:58:29 -0500

    make package installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="sd_bandits",  # Replace with your own username
+    version="0.0.1",
+    author="Aidan Claffey, Chris Ick, Dan Turkel, Johnny Ma",
+    author_email="",
+    description="Code ",
+    long_description="tktk",
+    # long_description_content_type="text/markdown",
+    url="https://github.com/daturkel/sd_bandits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
